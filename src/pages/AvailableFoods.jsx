181d85f36493a34b3465583e750379f63f9fb121
ii-shimul <islamshimul27@gmail.com>
2025-02-12 19:43:55 +0600

    fixed resonsivity

--- src/pages/AvailableFoods.jsx
+++ src/pages/AvailableFoods.jsx
@@ -56,7 +56,7 @@ const AvailableFoods = () => {
     });
     setAvailableFoods(filterBySearch);
     setHeader("Search Results");
-  }, [searchVal]);
+  }, [searchVal, perFoods]);
 
   if (isPending || isLoading) {
     <div
@@ -76,7 +76,29 @@ const AvailableFoods = () => {
         </h1>
         <p className="text-center mt-1 opacity-90">Find your favorite food</p>
       </div>
-      <div className="mb-6 mt-3 grid grid-cols-3 lg:grid-cols-5 gap-4">
+      <div className="sm:hidden mt-3">
+        <label className="input input-bordered flex items-center gap-2">
+          <input
+            onChange={(e) => setSearchVal(e.target.value)}
+            type="text"
+            className="grow"
+            placeholder="Search"
+          />
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 16 16"
+            fill="currentColor"
+            className="h-4 w-4 opacity-70"
+          >
+            <path
+              fillRule="evenodd"
+              d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
+              clipRule="evenodd"
+            />
+          </svg>
+        </label>
+      </div>
+      <div className="mb-6 mt-3 grid grid-cols-3 max-sm:grid-cols-2 lg:grid-cols-5 gap-4">
         <form onSubmit={handleLayout} className="flex items-center gap-2 w-fit">
           <select
             name="cols"
@@ -94,7 +116,7 @@ const AvailableFoods = () => {
             <LuLayoutGrid />
           </AwesomeButton>
         </form>
-        <div className="lg:col-span-3">
+        <div className="lg:col-span-3 max-sm:hidden">
           <label className="input input-bordered flex items-center gap-2">
             <input
               onChange={(e) => setSearchVal(e.target.value)}
@@ -118,7 +140,7 @@ const AvailableFoods = () => {
         </div>
         <div className="flex justify-end">
           <AwesomeButton onPress={() => handleSort()} className="w-fit ">
-            Sort by Date
+            Sort by Exp. Date
           </AwesomeButton>
         </div>
       </div>
@@ -136,7 +158,7 @@ const AvailableFoods = () => {
                   ? "grid-cols-4"
                   : ""
               }`
-            : "grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4"
+            : "grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4"
         }
       >
         {availableFoods?.map((food) => (
